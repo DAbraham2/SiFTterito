08434395559760e805d2e7f321a03915eb15de09
Daniel Abraham <daniel.abraham@edu.bme.hu>
2022-05-10 13:55:33 +0200

    remainder

--- src/server/SiFTServer.py
+++ src/server/SiFTServer.py
@@ -1,8 +1,9 @@
+import argparse
 import asyncio
-from socket import AF_INET
-from core import SiFTMainServer
-
 import logging
+from socket import AF_INET
+
+from core import SiFTMainServer
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
@@ -16,13 +17,27 @@ console.setFormatter(formatter)
 logging.getLogger('').addHandler(console)
 
 PORT = 5150
-#Requirement - serving clients on TCP port 5150
+# Requirement - serving clients on TCP port 5150
 
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--host', type=str, help='hostname to serve on')
+parser.add_argument('--port', type=int, help='A port to listen to')
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    serverLoop = loop.create_server(SiFTMainServer, 'localhost', PORT, family=AF_INET)
-    server = loop.run_until_complete(serverLoop)
-    print('Serving on {}'.format(server.sockets[0].getsockname()))
-    logging.info('Server startert on {}'.format(server.sockets[0].getsockname()))
+    host = ''
+    port = PORT
+    args = parser.parse_args()
+
+    if args.host:
+        host = args.host
+
+    if args.port:
+        port = args.port
+
+    logging.info(f'host: {host}\tport: {port}')
+    loop = asyncio.get_event_loop_policy().get_event_loop()
+    sLoop = loop.create_server(SiFTMainServer, host, port, family=AF_INET)
+    server = loop.run_until_complete(sLoop)
+    logging.info(f'Serving on {server.sockets[0].getsockname()}')
     loop.run_forever()
